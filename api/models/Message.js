e5512b00a894bb827f9775bbc4c194f64d15dcfc
lovevyas <love1252.be21@chitkarauniversity.edu.in>
2023-12-08 01:35:52 +0530

    Task completed

--- api/models/Message.js
+++ api/models/Message.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+
 const MessageSchema=new mongoose.Schema({
     sender:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
     recipient:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
